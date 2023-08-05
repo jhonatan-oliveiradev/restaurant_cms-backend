df918791a470b5303a6d2f2f166ecd664718b805
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-08-05 11:25:17 -0300

    :construction: feat: cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 
 import { router } from "./routes";
@@ -6,9 +7,19 @@ import "express-async-errors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {});
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	if (err instanceof Error) {
+		return res.status(400).json({ error: err.message });
+	}
+
+	return res.status(500).json({
+		status: "error",
+		message: "Internal Server Error"
+	});
+});
 
 app.listen(3333, () => {
 	console.log("Servidor online!");
